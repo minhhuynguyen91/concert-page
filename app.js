d18fd41d56a645058ecd3d2d759ea9af5f7bc713
Minh <ng.huyminh91@gmail.com>
2019-05-29 01:43:19 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express') 
+const app = express();
+const routes = require('./web/router');
+const bodyParser = require('body-parser');
+var path = require('path');
+var methodOverride = require('method-override');
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/simplemde', express.static(__dirname + '/node_modules/simplemde/dist'));
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);
+app.use('/concert/new', routes);
+app.use('/concerts', routes);
+app.use('/concerts/:id', routes);
+app.use('/concerts/:id/edit', routes);
+
+module.exports = app;
