adfae0eed93cfeecef05b442996c7ea8407c2936
Minh <ng.huyminh91@gmail.com>
2019-06-10 00:59:46 +0700

    Update the form

--- web/router/concert.js
+++ web/router/concert.js
@@ -18,6 +18,45 @@ exports.index = function(req, res) {
 
 };
 
+exports.post = function (req, res) {
+    [
+    body('title')
+      .isLength({min: 1})
+      .withMessage('Please enter the title'),
+
+    body('content')
+      .isLength({min: 1})
+      .withMessage('Please enter the content'),
+
+    body('img_link')
+      .isLength({min: 1})
+      .withMessage('Please enter the image link'),
+
+    body('Note')
+      .isLength({min: 1})
+      .withMessage('Please enter the image link')
+
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.body);
+    if (errors.isEmpty()) {
+      const concert = new Concert(req.body);
+      concert.save()
+        .then(() => { res.redirect('/'); })
+        .catch(() => { res.send('Sorry, something went wrong!'); });
+
+    } else {
+      res.render('concert/new', {
+        title: 'Create concert',
+        errors: errors.array(),
+        data: req.body
+      });
+    }
+  }
+};
+
+
 exports.new = function(req, res) {
   const concert = {title: '', content: ''};
   res.render('concert/new', {title: 'Create concert event', method: 'POST', action:'/concerts' , concert});
@@ -65,34 +104,4 @@ exports.id = function (req, res) {
     .catch(() => {
       res.send('something went wrong');
     });
-};
-
-exports.post = function (req, res) {
-    [
-    body('title')
-      .isLength({min: 1})
-      .withMessage('Please enter the title'),
-
-    body('content')
-      .isLength({min: 1})
-      .withMessage('Please enter the content')
-  ],
-
-  (req, res) => {
-    const errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      const concert = new Concert(req.body);
-      concert.save()
-        .then(() => { res.send('The concert is updated successfully'); })
-        .catch(() => { res.send('Sorry, something went wrong!'); });
-
-    } else {
-      res.render('concert/new', {
-        title: 'Create concert',
-        errors: errors.array(),
-        data: req.body
-      });
-    }
-  }
 };
