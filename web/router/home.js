02fe95b2b8246668fabbcede7418873b81fccc87
Minh <ng.huyminh91@gmail.com>
2019-06-11 10:08:22 +0700

    Update the card display

--- web/router/home.js
+++ web/router/home.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+const Concert = mongoose.model('Concert');
+
 exports.index = function(req, res) {
-  res.render('homes/index', {header: 'home'});
+  Concert.find()
+    .then((concerts) => {
+      res.render('homes/index', {header: 'home', concerts});
+    })
+    .catch((err) => {
+      res.send('Something went wrong!');
+    });
 };
 
 exports.contact = function(req, res) {
