b108baacf81fbc09303a7a743df75bbda6925d57
minhhuynguyen91 <ng.huyminh91@gmail.com>
2019-09-28 03:16:30 +0000

    Update the message

--- web/router/twilio_sms.js
+++ web/router/twilio_sms.js
@@ -12,21 +12,31 @@ exports.post = function(req, res) {
   var contentBody = contentBody.concat("Thong tin dat ve\nVo dien: ", req.body.title + "\n", "Ten: ", req.body.name + "\n", "So ve: ", req.body.ticket+"\n","sdt: ", req.body.phone +"\n", "ma cho ngoi: ", req.body.seatCode + "\n" );
   contentBody = contentBody.replace(/[^\x00-\x7F]/g, "");
   //console.log(contentBody);
-  //res.jsonp({success : true});
+  // res.jsonp({
+  //   success : true,
+  //   message: 'Chúng tôi đã nhận được phản hồi từ quý khách, chúng tôi sẽ gọi điện xác nhận trong thời gian sớm nhất'
+  // });
   
   client.messages
     .create({
-       body: contentBody,
-       from: '+1251290-8587',
-       to: '+84859456912'
+        body: contentBody,
+        from: '+1251290-8587',
+        to: '+84859456912'
+       //to: '+84908440968'
      })
     .then(() => {
-      res.jsonp({success : true})
+      res.jsonp({
+        success : true,
+        message: 'Chúng tôi đã nhận được phản hồi từ quý khách, chúng tôi sẽ gọi điện xác nhận trong thời gian sớm nhất'
+      })
     })
     
     .catch((err) => {
       console.log(err);
-      res.jsonp({error : true})
+      res.jsonp({
+        error : true,
+        message: 'Có lỗi từ hệ thống SMS. Xin quý khách liên hệ fanpage facebook của chúng tôi.'
+      })
     });
   
 };
